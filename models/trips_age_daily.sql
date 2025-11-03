42745f78e079fb84f01d92488b7661dcfc8c75c0
Ilya Shkolnikov <ex@192.168.0.16>
2025-11-03 10:03:25 +0300

    Add trips_users incremental model, refactor trips_age_daily

--- models/trips_age_daily.sql
+++ models/trips_age_daily.sql
@@ -1,15 +1,8 @@
-WITH
-    date_age_cte AS (SELECT
-                         date(t.started_at)                                                AS date
-                       , EXTRACT(YEAR FROM t.started_at) - EXTRACT(YEAR FROM u.birth_date) AS age
-                       , price_rub
-                     FROM {{ ref("trips_prep") }} AS t
-                              INNER JOIN {{ source("scooters_raw","users") }} AS u ON t.user_id = u.id)
 SELECT
     DATE
   , age
   , COUNT(*)   AS trips
   , SUM(price_rub) AS revenue_rub
-FROM date_age_cte
+FROM {{ ref("trips_users") }}
 GROUP BY 1,
          2
---
+++ models/trips_users.sql
@@ -0,0 +1,17 @@
+select
+     tp.*
+    ,u.sex
+    ,EXTRACT(YEAR FROM tp.started_at) - EXTRACT(YEAR FROM u.birth_date) AS age
+from {{ ref("trips_prep") }} tp
+left join {{ source("scooters_raw", "users") }} u on tp.user_id = u.id
+{% if is_incremental() %}
+    where
+        tp.id > (select max(id) from {{ this }})
+    order by
+        id
+    limit
+        75000
+{% else %}
+    where
+        tp.id <= 75000
+{% endif %}
