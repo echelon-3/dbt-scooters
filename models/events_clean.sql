103dcd8ab04012e2d9b8563412af2fa12e8796dd
Ilya Shkolnikov <ex@192.168.0.16>
2025-11-03 10:33:29 +0300

    Add events_clean incremental model

---
+++ models/events_clean.sql
@@ -0,0 +1,10 @@
+SELECT DISTINCT
+    user_id
+  , timestamp
+  , type_id
+FROM {{ source("scooters_raw","events") }}
+{% if is_incremental() %}
+    WHERE timestamp > (select max(timestamp) from {{ this }})
+{% else %}
+    WHERE date(timestamp) < date('2023-08-01')
+{% endif %}
