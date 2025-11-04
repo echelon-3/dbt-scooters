5ca032dcbfecfe9bd8560741ac79de0b11a7d066
Ilya Shkolnikov <exclusiverus@gmail.com>
2025-11-04 21:10:33 +0300

    Refactor events_clean model, add var to allow partial update

--- models/events_clean.sql
+++ models/events_clean.sql
@@ -1,11 +1,18 @@
+{% set date = var("date", none) %}
+
 SELECT DISTINCT
     user_id
   , timestamp
   , type_id
   , {{ updated_at() }}
 FROM {{ source("scooters_raw","events") }}
+WHERE
 {% if is_incremental() %}
-    WHERE timestamp > (select max(timestamp) from {{ this }})
+    {% if date %}
+        date(timestamp) = date('{{ date }}')
+    {% else %}
+        timestamp > (select max(timestamp) from {{ this }})
+    {% endif %}
 {% else %}
-    WHERE date(timestamp) < date('2023-08-01')
+    date(timestamp) < date('2023-08-01')
 {% endif %}
