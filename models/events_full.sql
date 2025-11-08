85343210f709d44eb85d0b454ef4f88804be4c93
Ilya Shkolnikov <exclusiverus@gmail.com>
2025-11-08 08:48:46 +0300

    Add events_stream ephemeral and book_scooter_retention models

--- models/events_full.sql
+++ models/events_full.sql
@@ -1,5 +1,8 @@
 SELECT
-     ec.*
+     ec.user_id
+    ,ec.type_id
     ,et.type
+    ,ec.timestamp
+    ,ec.updated_at
 FROM {{ ref("events_clean") }} ec
 LEFT JOIN {{ ref("event_types") }} et on ec.type_id=et.type_id
